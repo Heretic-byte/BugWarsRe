921aeca4a84c79220061b6f069b202c1995d24dd
marmot <wjdghwns9696@gmail.com>
2019-04-21 23:37:46 +0900

    latest

---
+++ BugFightRe/Assets/a_Scripts/CharcterEntityData/UnitBehavior/ubSkillCast.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ubSkillCast : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask _TargetLayer;
+    public LayerMask myTargetLayer { get => _TargetLayer; }
+    
+    //SkillAreaCast
+    //SkillTargeting
+
+    //Effect
+
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- BugFightRe/Assets/a_Scripts/So/Skills/SkillTarget.cs
+++ BugFightRe/Assets/a_Scripts/So/Skills/SkillTarget.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SkillTarget : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
